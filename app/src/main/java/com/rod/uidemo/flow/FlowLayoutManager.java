96607ee4c9c6783e68e99fdd9756427e8fb4e28e
RodZhang <zhangrd_fight@icloud.com>
2018-07-03 01:23:24 +0800

    初步回收view

--- app/src/main/java/com/rod/uidemo/flow/FlowLayoutManager.java
+++ app/src/main/java/com/rod/uidemo/flow/FlowLayoutManager.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Locale;
+
 /**
  * No pains, no gains.
  * <p>
@@ -12,6 +14,9 @@ import android.view.ViewGroup;
  */
 public class FlowLayoutManager extends RecyclerView.LayoutManager {
     private static final String TAG = "FlowLayoutManager";
+    private int mHorizontalSpace;
+    private int mVerticalSpace;
+    private int mScrollOffset;
 
     @Override
     public RecyclerView.LayoutParams generateDefaultLayoutParams() {
@@ -26,7 +31,49 @@ public class FlowLayoutManager extends RecyclerView.LayoutManager {
 
     @Override
     public int scrollVerticallyBy(int dy, RecyclerView.Recycler recycler, RecyclerView.State state) {
-        return super.scrollVerticallyBy(dy, recycler, state);
+
+        recycle(dy, recycler);
+        offsetChildrenVertical(-dy);
+        mScrollOffset += dy;
+        log("scrollVerticallyBy, dy=%d, mScrollOffset=%d", dy, mScrollOffset);
+        return dy;
+    }
+
+    private void recycle(int dy, RecyclerView.Recycler recycler) {
+        View childView;
+        if (dy < 0) {
+            for (int i = getChildCount() - 1; i >= 0; i--) {
+                childView = getChildAt(i);
+                if (getDecoratedTop(childView) - dy <= mVerticalSpace) {
+                    removeAndRecycleViews(recycler, getChildCount() - 1, i);
+                    return;
+                }
+            }
+        } else if (dy > 0) {
+            for (int i = 0, count = getChildCount(); i < count; i++) {
+                childView = getChildAt(i);
+                if (getDecoratedBottom(childView) + dy > 0) {
+                    removeAndRecycleViews(recycler, 0, i);
+                    return;
+                }
+            }
+        }
+    }
+
+    private void removeAndRecycleViews(RecyclerView.Recycler recycler, int startIndex, int endIndex) {
+        if (startIndex == endIndex) {
+            return;
+        }
+
+        if (endIndex > startIndex) {
+            for (int i = endIndex - 1; i >= startIndex; i--) {
+                removeAndRecycleViewAt(i, recycler);
+            }
+        } else {
+            for (int i = startIndex; i > endIndex; i--) {
+                removeAndRecycleViewAt(i, recycler);
+            }
+        }
     }
 
     @Override
@@ -35,22 +82,36 @@ public class FlowLayoutManager extends RecyclerView.LayoutManager {
             return;
         }
         detachAndScrapAttachedViews(recycler);
-        int horizontalSpace = getWidth() - getPaddingLeft() - getPaddingRight();
 
-        View child;
+        mHorizontalSpace = getWidth() - getPaddingLeft() - getPaddingRight();
+        mVerticalSpace = getHeight() - getPaddingTop() - getPaddingBottom();
+        log("onLayoutChildren, mHorizontalSpace=%d, mVerticalSpace=%d", mHorizontalSpace, mVerticalSpace);
+
+        View itemView;
         int startX = getPaddingLeft();
         int startY = getPaddingTop();
         for (int i = 0, size = state.getItemCount(); i < size; i++) {
-            child = recycler.getViewForPosition(i);
-            addView(child);
-            measureChildWithMargins(child, 0, 0);
-            if (startX + getDecoratedMeasuredWidth(child) > horizontalSpace) {
+            itemView = recycler.getViewForPosition(i);
+            addView(itemView);
+            measureChildWithMargins(itemView, 0, 0);
+            if (startX + getDecoratedMeasuredWidth(itemView) > mHorizontalSpace) {
                 startX = getPaddingLeft();
-                startY += getDecoratedMeasuredHeight(child);
+                startY += getDecoratedMeasuredHeight(itemView);
+                if (startY > mVerticalSpace) {
+                    detachAndScrapView(itemView, recycler);
+                    log("index=%d, startY(%d) > mVerticalSpace(%d), end", i, startY, mVerticalSpace);
+                    break;
+                }
             }
-            layoutDecoratedWithMargins(child, startX, startY, startX + getDecoratedMeasuredWidth(child), startY + getDecoratedMeasuredHeight(child));
+            layoutDecoratedWithMargins(itemView, startX, startY, startX + getDecoratedMeasuredWidth(itemView), startY + getDecoratedMeasuredHeight(itemView));
             Log.d(TAG, "startX=" + startX + ", startY=" + startY);
-            startX += getDecoratedMeasuredWidth(child);
+            log("index=%d, startX=%d, startY=%d", i, startX, startY);
+            startX += getDecoratedMeasuredWidth(itemView);
+
         }
+    }
+
+    private void log(String format, Object... args) {
+        Log.d(TAG, String.format(Locale.getDefault(), format, args));
     }
 }
