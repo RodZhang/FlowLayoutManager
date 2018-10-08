337b83f3eadfa71ed86ad8ff99f569100166bbd3
RodZhang <zhangrd_fight@icloud.com>
2018-10-08 16:58:10 +0800

    test Thread.join(), Multiply Thread print ABC

--- app/src/test/java/com/rod/uidemo/ExampleUnitTest.kt
+++ app/src/test/java/com/rod/uidemo/ExampleUnitTest.kt
@@ -14,4 +14,47 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun test_printABC() {
+        val a = Object()
+        val b = Object()
+        val c = Object()
+
+        val t1 = PrintThread("A", c, a)
+        val t2 = PrintThread("B", a, b)
+        val t3 = PrintThread("C", b, c)
+
+        println("start")
+        t1.start()
+        Thread.sleep(100)
+        t2.start()
+        Thread.sleep(100)
+        t3.start()
+
+        t3.join()
+        print("\nend")
+    }
+
+    class PrintThread(val content: String, val pre: Object, val self: Object) : Thread() {
+
+        override fun run() {
+            var count = 10;
+            while (count > 0) {
+                synchronized(pre) {
+                    synchronized(self) {
+                        count--
+                        print(content)
+                        self.notify()
+                    }
+
+
+                    if (count == 0) {
+                        return
+                    }
+                    pre.wait()
+                }
+            }
+        }
+    }
 }
